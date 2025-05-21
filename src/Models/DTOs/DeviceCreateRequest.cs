bfe772f9672c1d9f3764ad291f814b8352224782
aniaania321 <s31154@pjwstk.edu.pl>
2025-05-21 19:18:20 +0200

    Added files to gitignore and I also fixed the Personid1 issue (there was a problem with the generated models, they had two properties 'employees' for some reason)

--- src/Models/DTOs/DeviceCreateRequest.cs
+++ src/Models/DTOs/DeviceCreateRequest.cs
@@ -6,8 +6,11 @@ public class DeviceCreateRequest
 {
     [Required]
     public string Name { get; set; }
+    [Required]
     public bool IsEnabled { get; set; }
+    [Required]
     public Dictionary<string, string> AdditionalProperties { get; set; }
+    [Required]
     public int? DeviceTypeId { get; set; }
 
     
