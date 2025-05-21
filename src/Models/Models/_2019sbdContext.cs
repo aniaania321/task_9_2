79200dc816bff3cd29dac6b4c66496e1eaf4841e
aniaania321 <s31154@pjwstk.edu.pl>
2025-05-21 18:44:18 +0200

    removed connection string(sorry i though You meant to not write it in readme file)

--- src/Models/Models/_2019sbdContext.cs
+++ src/Models/Models/_2019sbdContext.cs
@@ -27,11 +27,6 @@ public partial class _2019sbdContext : DbContext
     public virtual DbSet<Person> People { get; set; }
 
     public virtual DbSet<Position> Positions { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=db-mssql;Initial Catalog=2019SBD;Integrated Security=True;Trust Server Certificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("s31154");
