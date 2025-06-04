2f7f58eb67375ad5855929d76d2e47c3787942d5
aniaania321 <s31154@pjwstk.edu.pl>
2025-06-04 19:29:32 +0200

    included migrations

---
+++ src/API/Migrations/20250602205037_role_user.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class role_user : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "s31154");
+
+            migrationBuilder.CreateTable(
+                name: "DeviceType",
+                schema: "s31154",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                schema: "s31154",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PassportNumber = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: false),
+                    FirstName = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    MiddleName = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    LastName = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    Email = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Position",
+                schema: "s31154",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    MinExpYears = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Position", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                schema: "s31154",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Device",
+                schema: "s31154",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: false),
+                    IsEnabled = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    AdditionalProperties = table.Column<string>(type: "varchar(8000)", unicode: false, maxLength: 8000, nullable: false, defaultValue: ""),
+                    DeviceTypeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Device", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Device_DeviceType",
+                        column: x => x.DeviceTypeId,
+                        principalSchema: "s31154",
+                        principalTable: "DeviceType",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                schema: "s31154",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PositionId = table.Column<int>(type: "int", nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    HireDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "(sysutcdatetime())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employee_Person",
+                        column: x => x.PersonId,
+                        principalSchema: "s31154",
+                        principalTable: "Person",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Employee_Position",
+                        column: x => x.PositionId,
+                        principalSchema: "s31154",
+                        principalTable: "Position",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Account",
+                schema: "s31154",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Account_Employee_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalSchema: "s31154",
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Account_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalSchema: "s31154",
+                        principalTable: "Role",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeviceEmployee",
+                schema: "s31154",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeviceId = table.Column<int>(type: "int", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    IssueDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "(sysutcdatetime())"),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceEmployee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeviceEmployee_Device",
+                        column: x => x.DeviceId,
+                        principalSchema: "s31154",
+                        principalTable: "Device",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_DeviceEmployee_Employee",
+                        column: x => x.EmployeeId,
+                        principalSchema: "s31154",
+                        principalTable: "Employee",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_EmployeeId",
+                schema: "s31154",
+                table: "Account",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_RoleId",
+                schema: "s31154",
+                table: "Account",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_Username",
+                schema: "s31154",
+                table: "Account",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Device_DeviceTypeId",
+                schema: "s31154",
+                table: "Device",
+                column: "DeviceTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceEmployee_DeviceId",
+                schema: "s31154",
+                table: "DeviceEmployee",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceEmployee_EmployeeId",
+                schema: "s31154",
+                table: "DeviceEmployee",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__DeviceTy__737584F67ED4B33A",
+                schema: "s31154",
+                table: "DeviceType",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_PersonId",
+                schema: "s31154",
+                table: "Employee",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_PositionId",
+                schema: "s31154",
+                table: "Employee",
+                column: "PositionId");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Person__45809E71B07F07BB",
+                schema: "s31154",
+                table: "Person",
+                column: "PassportNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Person__85FB4E380F4F916A",
+                schema: "s31154",
+                table: "Person",
+                column: "PhoneNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Person__A9D10534D23E2481",
+                schema: "s31154",
+                table: "Person",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Position__737584F69C41767E",
+                schema: "s31154",
+                table: "Position",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Role_Name",
+                schema: "s31154",
+                table: "Role",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Account",
+                schema: "s31154");
+
+            migrationBuilder.DropTable(
+                name: "DeviceEmployee",
+                schema: "s31154");
+
+            migrationBuilder.DropTable(
+                name: "Role",
+                schema: "s31154");
+
+            migrationBuilder.DropTable(
+                name: "Device",
+                schema: "s31154");
+
+            migrationBuilder.DropTable(
+                name: "Employee",
+                schema: "s31154");
+
+            migrationBuilder.DropTable(
+                name: "DeviceType",
+                schema: "s31154");
+
+            migrationBuilder.DropTable(
+                name: "Person",
+                schema: "s31154");
+
+            migrationBuilder.DropTable(
+                name: "Position",
+                schema: "s31154");
+        }
+    }
+}
